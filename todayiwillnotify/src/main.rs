97feffbed1303b5f6c43337e26d0904881eedf07
Vinicius <vmayrink12@gmail.com>
2024-07-01 00:42:25 +0000

    Basic watch-reminder ok

--- todayiwillnotify/src/main.rs
+++ todayiwillnotify/src/main.rs
@@ -1,12 +1,14 @@
 use daemonize::Daemonize;
 use env_logger::Env;
 use log::{error, info};
-use notify::{Config, Event, RecommendedWatcher, RecursiveMode, Result, Watcher};
+use notify::{Config, RecommendedWatcher, RecursiveMode, Result, Watcher};
 use notify_rust::Notification;
-use std::thread;
+use std::sync::{Arc, Mutex};
+use std::{process, thread};
 use std::time::Duration;
 use std::{fs::File, sync::mpsc::channel};
 use todayiwill::appointment::{self, Config as TiwConfig};
+use todayiwill::AppointmentTime;
 
 extern crate dirs;
 
@@ -14,16 +16,22 @@ fn main() -> Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     let daemonize = Daemonize::new()
-        .pid_file("/tmp/file_watcher_daemon.pid")
+        .pid_file("/tmp/todayiwillnotify-daemon.pid")
         .chown_pid_file(true)
         .working_directory("/tmp")
-        .stdout(File::create("/tmp/file_watcher_daemon.out").unwrap())
-        .stderr(File::create("/tmp/file_watcher_daemon.err").unwrap());
+        .stdout(File::create("/tmp/todayiwillnotify-daemon.out").unwrap())
+        .stderr(File::create("/tmp/todayiwillnotify-daemon.err").unwrap());
 
     match daemonize.start() {
         Ok(_) => info!("Daemonized successfully"),
-        Err(e) => error!("Error daemonizing: {}", e),
+        Err(e) => {
+            error!("Error daemonizing: {}. Process exited", e);
+            process::exit(1);
+        }
     }
+
+    let appointments = Arc::new(Mutex::new(vec![]));
+    let appointments_clone = appointments.clone();
 
     let (tx, rx) = channel();
     let mut watcher: RecommendedWatcher = Watcher::new(
@@ -39,28 +47,47 @@ fn main() -> Result<()> {
 
     thread::spawn(move || loop {
         match rx.recv() {
-            Ok(event) => handle_event(event.unwrap()),
+            Ok(event) => {
+                let mut appointments = appointments_clone.lock().unwrap();
+                info!("Event: {:?}", event);
+                let file_appointments = appointment::list::get_appointments_from_file(
+                    &TiwConfig::default().appointment_file_path_current_day,
+                );
+                *appointments = file_appointments
+                    .into_iter()
+                    .filter(|appointment| appointment.time > AppointmentTime::now())
+                    .collect();
+                info!("Appointments updated")
+            }
             Err(e) => error!("watch error: {:?}", e),
         }
+    });
+
+    thread::spawn(move || loop {
+        let mut appointments = appointments.lock().unwrap();
+        appointments.retain(|appointment| {
+            if appointment.time > AppointmentTime::now()
+                && AppointmentTime::now() + 1 >= appointment.time
+            {
+                let notification_result = Notification::new()
+                    .summary("Reminder")
+                    .body(&appointment.description)
+                    .show();
+                match notification_result {
+                    Ok(..) => info!("Appointment \"{appointment}\" notified"),
+                    Err(e) => error!("Failed to notify {appointment}. Error {e}"),
+                }
+                false
+            } else {
+                true
+            }
+        });
+        info!("Appointments left to notify: {}", appointments.len());
+        drop(appointments);
+        thread::sleep(Duration::from_secs(20));
     });
 
     loop {
         thread::sleep(Duration::from_secs(60));
     }
-}
-
-// Function to handle events.
-fn handle_event(event: Event) {
-    info!("Event: {:?}", event);
-    let appointments = appointment::list::get_appointments_from_file(
-        &TiwConfig::default().appointment_file_path_current_day,
-    );
-    for appointment in appointments {
-        info!("Appointment found: {:?}", appointment);
-    }
-    Notification::new()
-        .summary("Appointment change")
-        .body("File change")
-        .show()
-        .expect("Failed to display message");
 }
